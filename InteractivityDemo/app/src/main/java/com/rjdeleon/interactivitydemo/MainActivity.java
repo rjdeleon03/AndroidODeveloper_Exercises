98b8a9dde1e899565496140219f657dfdf688cdb
DESKTOP-5FEF2FS <ricardopablo.deleon@gmail.com>
2018-03-21 20:55:10 +0800

    Add LoginDemo project

--- InteractivityDemo/app/src/main/java/com/rjdeleon/interactivitydemo/MainActivity.java
+++ InteractivityDemo/app/src/main/java/com/rjdeleon/interactivitydemo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickFunction(View view) {
-        Log.i("info", "button pressed!");
+
+        EditText nameEditText = (EditText)findViewById(R.id.nameEditText);
+        Log.i("Info", "button pressed!");
+        Log.i("Values", nameEditText.getText().toString());
     }
 }
